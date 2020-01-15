c7cf41cbcb2ce62c18a8ece930c63c31867d28fd
Laci <ls@gerhardtinformatics.com>
2020-01-15 11:13:13 +0100

    Add initial file from Sanyi

---
+++ hofokkulonbseg_kijelzovel_sajat_2_jo_KK_i2c.ino
@@ -0,0 +1,118 @@
+//reuk.co.uk November 2013
+
+//for information and the schematic circuit diagram for this
+//http://www.reuk.co.uk/wordpress/arduino-solar-water-heating-pump-controller-design-and-code/
+
+// SZUKSEGES KONYVTARAK:
+// LiquidCrystal.h -> LiquidCrystal add-in by Arduino
+
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+
+ 
+const int PANELSENSOR = A1;
+const int TANKSENSOR = A0;
+const int RELAY = 13;
+
+const int diffON = 3;
+const int diffOFF = 2;
+int x = 0;
+String y = "KI";
+String z = "BE";
+float solarPanelTemperature = 0.0;
+float hotWaterTankTemperature = 0.0;
+LiquidCrystal_I2C lcd(0x27,20,4);
+
+void setup(){
+  lcd.init();                      // initialize the lcd 
+  lcd.backlight();
+  
+ 
+  lcd.home();
+
+
+  Serial.begin(9600); 
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD
+  lcd.backlight();
+  lcd.print("HOMERES INDITASA...");
+  pinMode(PANELSENSOR, INPUT);
+  pinMode(TANKSENSOR, INPUT);
+  
+  pinMode(RELAY, OUTPUT);
+  digitalWrite(RELAY, LOW);
+  delay(1000);
+  lcd.clear();
+   
+}
+
+
+void loop(){
+  // homeresek és skálázások
+  Serial.println("HOMERES");
+   solarPanelTemperature = analogRead(PANELSENSOR) * (92.0 / 1023.0);
+   hotWaterTankTemperature = analogRead(TANKSENSOR) * (92.0 / 1023.0);
+  
+ // ha feltétel teljesül, bekapcsolás        
+   if(solarPanelTemperature > (hotWaterTankTemperature + diffON)){
+    Serial.println("relay bekapcsolva");
+     digitalWrite(RELAY, HIGH);
+     x = 1;
+    
+   
+   
+    
+   
+     
+    
+   }
+ // ha a feltétel teljesül, akkor kikapcsolás  
+   if(x == 1 && solarPanelTemperature <= hotWaterTankTemperature + diffOFF)
+   {
+    digitalWrite(RELAY, LOW);
+     x = 0;
+    
+ 
+     Serial.println(" ki");
+    
+   }
+
+    
+ 
+
+  
+   delay(250);
+  
+// Hőmérséklet a kijelzőn
+// ----------------
+// KIJELZO ELSO SORA
+lcd.setCursor(0, 0);
+lcd.print("kazan  hofok: ");// a Temp sablon kiiratás
+// lcd.print esetén az első adat az oszlop (0-val kezd), a második adat a sor (0-val kezd ez is)
+lcd.setCursor(14, 0);// a kurzor pozicionálása. 7 = 8. oszlop. 0 = első sor
+lcd.print(solarPanelTemperature);
+
+// KIJELZO MASODIK SORA
+lcd.setCursor(0, 1);// a kurzor pozicionálása. 0 = első oszlop. 1 = második sor
+lcd.print("tarolo hofok: ");// a Temp sablon kiiratás
+lcd.setCursor(14, 1);
+lcd.print(hotWaterTankTemperature);
+
+// KIJELZO HARMADIK SORA
+lcd.setCursor(0, 2);// a kurzor pozicionálása. 0 = első oszlop. 1 = második sor
+lcd.print("szivattyu: ");// a Temp sablon kiiratás
+lcd.setCursor(14, 2);
+if(x == 0){
+  lcd.print("KI");
+}else{
+  lcd.print("BE");
+}
+
+
+
+
+
+delay(500);// fél másodperc várakozás
+}
